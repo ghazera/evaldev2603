6ad188da147e0d31664f8c94b7002b3ecc7bce6d
ghazera <noamie.loveyt@gmail.com>
2026-09-18 13:51:03 +0200

    Ajout du formulaire de connexion sur le fichier ViewConnexion.php

---
+++ App/View/ViewConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./public/assets/scripts/main.js" defer></script>
+    <link rel="stylesheet" href="./public/assets/styles/main.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form action="" method="post">
+        <label for="email">Veuillez saisir votre adresse email :</label>
+        <input type="email" name="email">
+        <label for="pswd">Veuillez saisir mdp :</label>
+        <input type="pswd" name="pswd">
+        <input type="submit" name="submit" value="connexion">
+    </form>
+</body>
+</html>
